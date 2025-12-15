1d3bc3223291c3a5ffebe76669bbf352938bcd85
TitouanCharrier <70266642+TitouanCharrier@users.noreply.github.com>
2025-12-15 21:24:11 +0100

    feat: ajout du workflow deploy

--- localxchange/vite.config.ts
+++ localxchange/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
+const repoName = 'M1_GL_Projet'; 
+
 export default defineConfig({
   plugins: [svelte()],
-})
+  
+  base: `/${repoName}/localxchange/`, 
+  
+});
