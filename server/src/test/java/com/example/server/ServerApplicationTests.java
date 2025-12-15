9b393fa0c1f2524c286bfb12d7e9bdf2f5b61b3c
Titouan Charrier <titouan@charrier.ovh>
2025-12-15 16:37:34 +0100

    feat: ajout des tests junit

--- server/src/test/java/com/example/server/ServerApplicationTests.java
+++ server/src/test/java/com/example/server/ServerApplicationTests.java
@@ -2,12 +2,34 @@ package com.example.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ServerApplicationTests {
 
+    @Test
+    void contextLoads() {
+        // Le test réussit si le contexte Spring Boot est chargé sans exception.
+    }
+
 	@Test
-	void contextLoads() {
+	void uselessTest1() {
+        assertTrue(true);
+	}
+
+	@Test
+	void uselessTest2() {
+        assertTrue(true);
+	}
+
+	@Test
+	void uselessTest3() {
+        assertTrue(true);
+	}
+    
+	@Test
+	void uselessTest4() {
+        assertTrue(true);
 	}
 
 }
