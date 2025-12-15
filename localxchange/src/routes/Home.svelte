dadf6c2f25c6ec098145267cd7461a93c7a8a694
TitouanCharrier <70266642+TitouanCharrier@users.noreply.github.com>
2025-12-15 23:29:39 +0100

    Update fetch URL for resources endpoint

--- localxchange/src/routes/Home.svelte
+++ localxchange/src/routes/Home.svelte
@@ -8,7 +8,7 @@
     let resources: any[] | undefined;
     let filteredResources: any[] | undefined;
     
-    fetch('https://78.114.8.86:8080/resources')  
+    fetch('https://78.114.8.86/resources')  
         .then(response => {
             if (!response.ok) {
                 throw new Error('Erreur HTTP ' + response.status);
